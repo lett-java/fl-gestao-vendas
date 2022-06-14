6ea89dc691423070ceeb5bf46658fafb5114852e
Fabio Lettieri <fabiolettieri09@gmail.com>
2022-06-14 11:02:24 -0300

    Remoção de interface não utilizada

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -3,12 +3,6 @@ import { getCustomRepository } from "typeorm"
 import { ProductRepository } from "../repository/ProductRepository"
 import Product from "../typeorm/entities/Product";
 
-interface IRequest {
-    name: string,
-    price: number,
-    quantity: number
-}
-
 class ListProductService {
 
     public async execute(): Promise<Product[]> {
